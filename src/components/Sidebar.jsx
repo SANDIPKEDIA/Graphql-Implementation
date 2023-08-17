25114c3771876bfca005cc24924887956efac876
Dipankar <dipankar.db37@gmail.com>
2023-08-17 14:47:37 +0530

    responsive avatar add

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,10 +6,11 @@ import { SONG_PLAYLIST_TYPE } from "../utils/reserved-values";
 
 const Sidebar = ({ handleChangeSongCategory, currentSongType }) => {
   const [toggle, settoggle] = useState(false);
+  
   return (
     <>
       <button
-        className="my_btn_2 responsive_btn "
+        className="my_btn_2 responsive_btn burger_btn"
         onClick={() => settoggle(!toggle)}
       >
         <LIST_SVG />
@@ -46,14 +47,14 @@ const Sidebar = ({ handleChangeSongCategory, currentSongType }) => {
                 );
               })}
             </ul>
-            <div className="avatar">
+          </div>
+        )}
+        <div className="responsive_avatar">
               <img
                 src="https://images.unsplash.com/photo-1691349202760-b139b5238a76?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=386&q=80"
                 alt=""
               />
             </div>
-          </div>
-        )}
 
         <div className="computer_nav">
           <span className="nav_header">
